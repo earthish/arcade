82c82585534d5314711dfc1ab1d9efbe7b31bc01
earthish <prithvish04@gmail.com>
2025-10-27 16:03:46 +0530

    final

--- arcade_menu.py
+++ arcade_menu.py
@@ -6,11 +6,11 @@ def arcade_menu():
     # Map menu numbers to game functions
     game_switch = {
         "1": hand_cricket.main,
-        "2": hangman.play_round,            
+        "2": hangman.main,            
         "3": battleship.battleship,
         "4": blackjack.play_blackjack,
         "5": typingspeed.typing_speed_game,
-        "6": rock_paper_scissors.play_rps, 
+        "6": rock_paper_scissors.play_rps,
         "7": tictactoe.play_tictactoe
     }
 
